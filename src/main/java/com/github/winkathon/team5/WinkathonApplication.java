71f2d2c170922d667144f98a6c522f919e18c9ca
Son Daehyeon <sondaehyeon01@gmail.com>
2024-08-29 17:09:37 +0900

    feat(swagger): Swagger 적용

--- src/main/java/com/github/winkathon/team5/WinkathonApplication.java
+++ src/main/java/com/github/winkathon/team5/WinkathonApplication.java
@@ -7,8 +7,17 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import com.github.winkathon.team5.common.property.MongoProperty;
 import com.github.winkathon.team5.common.property.RedisProperty;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @EnableConfigurationProperties({MongoProperty.class, RedisProperty.class})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Winkathon Team 5",
+                description = "2024년도 국민대학교 소프트웨어융합대학 웹 학술 동아리 Wink 해커톤 - 5팀"
+        )
+)
 public class WinkathonApplication {
 
     public static void main(String[] args) {
