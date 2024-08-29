8c5eb9c8b973cce3f900b20b76bcdd70fb12fd18
Son Daehyeon <sondaehyeon01@gmail.com>
2024-08-29 16:39:20 +0900

    feat: 설정 파일 분리

--- src/main/java/com/github/winkathon/team5/WinkathonApplication.java
+++ src/main/java/com/github/winkathon/team5/WinkathonApplication.java
@@ -2,11 +2,17 @@ package com.github.winkathon.team5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.github.winkathon.team5.common.property.MongoProperty;
+import com.github.winkathon.team5.common.property.RedisProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties({MongoProperty.class, RedisProperty.class})
 public class WinkathonApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WinkathonApplication.class, args);
     }
 }
