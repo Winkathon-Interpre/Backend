e27d6db5adac957e912f248d4ae1649a8576bfd7
Son Daehyeon <sondaehyeon01@gmail.com>
2024-08-29 15:00:40 +0900

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/github/winkathon/team5/WinkathonApplication.java
@@ -0,0 +1,12 @@
+package com.github.winkathon.team5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinkathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinkathonApplication.class, args);
+    }
+}
