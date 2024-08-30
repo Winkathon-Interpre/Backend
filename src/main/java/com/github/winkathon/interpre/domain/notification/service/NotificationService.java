15a683e643f4573d49a6dd446890b795607e9d1e
Son Daehyeon <sondaehyeon01@gmail.com>
2024-08-30 21:33:38 +0900

    feat: Notification 구현 (#9)
    * refactor(credit): 서비스의 slf4j 제거
    
    * feat: Notification 구현

---
+++ src/main/java/com/github/winkathon/interpre/domain/notification/service/NotificationService.java
@@ -0,0 +1,63 @@
+package com.github.winkathon.interpre.domain.notification.service;
+
+import static java.time.LocalDateTime.*;
+
+import java.util.List;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.github.winkathon.interpre.domain.notification.dto.response.GetNotificationsResponse;
+import com.github.winkathon.interpre.domain.notification.exception.NotificationNotFoundException;
+import com.github.winkathon.interpre.domain.notification.repository.NotificationRepository;
+import com.github.winkathon.interpre.domain.notification.schema.Notification;
+import com.github.winkathon.interpre.domain.user.schema.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+    public GetNotificationsResponse getNotifications(User user) {
+
+        List<Notification> notifications = notificationRepository.findByOwner(user);
+
+        return GetNotificationsResponse.builder()
+                .notifications(notifications)
+                .build();
+    }
+
+    public void readNotification(User user, String notificationId) {
+
+        Notification notification = notificationRepository.findById(notificationId)
+                .orElseThrow(NotificationNotFoundException::new);
+
+        if (!notification.getOwner().equals(user)) {
+
+            throw new NotificationNotFoundException();
+        }
+
+        notification.setRead(true);
+
+        notificationRepository.save(notification);
+    }
+
+    public void readAllNotification(User user) {
+
+        notificationRepository.findByOwner(user).stream()
+                .peek(notification -> notification.setRead(true))
+                .forEach(notificationRepository::save);
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    private void deleteOldNotification() {
+
+        notificationRepository.findAll().stream()
+                .filter(Notification::isRead)
+                .filter(notification -> notification.getCreatedAt().isBefore(now().minusDays(7)))
+                .forEach(notificationRepository::delete);
+    }
+}
