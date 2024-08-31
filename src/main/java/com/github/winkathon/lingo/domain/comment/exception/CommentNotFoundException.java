afd0a0a08376bfa6e141ee1d523eb4ba5bb0f7b6
Son Daehyeon <sondaehyeon01@gmail.com>
2024-08-31 20:07:36 +0900

    feat: Comment 구현 (#16)

---
+++ src/main/java/com/github/winkathon/lingo/domain/comment/exception/CommentNotFoundException.java
@@ -0,0 +1,13 @@
+package com.github.winkathon.lingo.domain.comment.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.github.winkathon.lingo.common.api.exception.ApiException;
+
+public class CommentNotFoundException extends ApiException {
+
+    public CommentNotFoundException() {
+
+        super(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다.");
+    }
+}
